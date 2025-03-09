fab9d49465edccc9aa6a0e15acdaa81f5a90bb16
John Luke <john41x4@gmail.com>
2025-03-08 19:43:30 -0600

    Fixed text

--- data/Index.ts
+++ data/Index.ts
@@ -8,7 +8,7 @@ export const navItems = [
   export const gridItems = [
     {
       id: 1,
-      title: "I strive to stand at the forefront of innovation",
+      title: "I Strive to Stand at the Forefront of Innovation",
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
@@ -18,7 +18,7 @@ export const navItems = [
     },
     {
       id: 2,
-      title: "Transforming challenges into Breakthroughs",
+      title: "Transforming Challenges into Breakthroughs",
       description: "",
       className: "lg:col-span-2 md:col-span-3 md:row-span-2",
       imgClassName: "",
@@ -28,8 +28,8 @@ export const navItems = [
     },
     {
       id: 3,
-      title: "My tech stack",
-      description: "I am always improving",
+      title: "My Tech Stack",
+      description: "I am Always Improving",
       className: "lg:col-span-2 md:col-span-3 md:row-span-2",
       imgClassName: "",
       titleClassName: "justify-center",
@@ -38,7 +38,7 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Maximize the user experience",
+      title: "Maximize the User Experience",
       description: "My Goal is to",
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
@@ -49,8 +49,8 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building an AI powered workout tracker",
-      description: "Behind the scenes",
+      title: "Currently Building an AI Powered Workout Tracker",
+      description: "Behind the Scenes",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
@@ -59,7 +59,7 @@ export const navItems = [
     },
     {
       id: 6,
-      title: "Let's work on something together!",
+      title: "Let's Work on Something Together!",
       description: "",
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
